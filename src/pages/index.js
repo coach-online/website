9e207480faa9cdae5b0d117361269095cb6aab2c
ramy <ramyshurafa@hotmail.com>
2020-07-18 17:44:52 +0300

    create theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
 
 import Header from '../components/Header';
 import SEO from '../components/seo';
 import HeroSection from '../components/HeroSection';
 
-const IndexPage = () => (
-  <>
-    <Header />
-    <SEO title="Home" />
-    <HeroSection />
-  </>
-);
+const useStyles = createUseStyles((theme) => ({
+  paragraph: {
+    color: theme.colors.blue,
+  },
+}));
+
+const IndexPage = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Header />
+      <SEO title="Home" />
+      <HeroSection />
+      <p className={classes.paragraph}>test</p>
+    </>
+  );
+};
 
 export default IndexPage;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,16 @@
 const theme = {
-  fontSize: 16,
-  fontFamily: "Roboto",
-  color: "#212121",
-}
+  fontFamily: 'Almarai',
+  colors: {
+    black: '#3a3a3a',
+    gray: '#f7f9fe', // section background
+    blue: '#138afd',
+    pink: '#f6716f',
+  },
+  breakpoints: {
+    laptop: '1024px',
+    tablet: '767px',
+    mobile: '520px',
+  },
+};
 
 module.exports = theme;
