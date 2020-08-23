556ff826aa5d8d88902c87fad9f853fd50cb5204
israa sulaiman <israa96sulaiman@gmail.com>
2020-08-23 13:03:12 +0300

    add a function to get feature image by name

--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -13,6 +13,7 @@ export default function MainFeaturesSection() {
         edges {
           node {
             id
+            name
             childImageSharp {
               fluid(maxWidth: 600) {
                 ...GatsbyImageSharpFluid
@@ -23,34 +24,37 @@ export default function MainFeaturesSection() {
       }
     }
   `);
-  const images = data.images ? data.images.edges : null;
+  const getFeatureImgSrc = (name) => (data.images
+    ? data.images.edges.find((edge) => edge.node.name === name).node
+      .childImageSharp.fluid
+    : null);
   return (
     <>
       <section id="features" className={featuresStyles.features}>
         <Container>
           <h2 className="font-style-4 center">مزايا كوتش أونلاين</h2>
           <Feature
-            src={images && images[0].node.childImageSharp.fluid}
+            src={getFeatureImgSrc('feature1')}
             alt="feature 1"
             p="كوتش اونلاين يساعدك على تنظيم جداول التغذية والتمرين للمشتركين الخاصين بك بسهولة تامة عن طريق اعادة استخدام الجداول التي قمت بانشائها سابقا"
             h3="توفير الوقت والجهد"
           />
           <Feature
             order="-1"
-            src={images && images[1].node.childImageSharp.fluid}
+            src={getFeatureImgSrc('feature2')}
             alt="feature 2"
             p=" كوتش اونلاين يتيح لك مجموعة كبيرة من جداول التمرين والتغذية المعتمدة ويمكنك ايضا التعديل عليها وتخصيصها للمشترك الجديد"
             h3="تنظيم جداول المشتركين"
           />
           <Feature
-            src={images && images[2].node.childImageSharp.fluid}
+            src={getFeatureImgSrc('feature3')}
             alt="feature 2"
             p=" كوتش اونلاين يوفر لك قائمة كبيرة من الوجبات الغذائية التي يمكنك  استخدامها في بناء جداول التغذية بالاضافة لتفاصيل والمكونات الغذائية لكل وجبة"
             h3="بناء جداول التغذية"
           />
           <Feature
             order="-1"
-            src={images && images[3].node.childImageSharp.fluid}
+            src={getFeatureImgSrc('feature4')}
             alt="feature 2"
             p=" كوتش اونلاين يوفر لك مكتبة فيديو ضخمة من التمارين مع شرح طريقة اداء كل تمارين يمكنك استخدامها في بناء جداول المشتركين"
             h3="بناء جداول التمرين"
