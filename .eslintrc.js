0730266ffa7b8d2aa5b4804980c45d1bc01e8d11
Ahmed Shatat <a.shatat@hotmail.com>
2020-09-04 11:28:19 +0300

    set up github action
    #20

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,15 @@ module.exports = {
   ],
   rules: {
     'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'jsx-a11y/label-has-associated-control': [
+      'error', {
+        labelComponents: [],
+        labelAttributes: [],
+        controlComponents: [],
+        assert: 'both',
+        depth: 25,
+      },
+    ],
   },
 };
